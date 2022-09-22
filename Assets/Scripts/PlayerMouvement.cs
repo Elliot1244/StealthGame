e263472fc788806c278a1959254984e786ca0396
Elliot <elliot.lenoa@laposte.net>
2022-09-22 15:05:54 +0200

    movement player and rotation + animation walk/run

---
+++ Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMouvement : MonoBehaviour
+{
+    [SerializeField] InputActionReference _movement;
+    [SerializeField] InputActionReference _sprint;
+    [SerializeField] Animator _animator;
+    [SerializeField] CharacterController _controller;
+    [SerializeField] Camera _camera;
+    [SerializeField] int _isWalkingAnim;
+    [SerializeField] float _speed;
+    [SerializeField] float _gravity;
+    //[SerializeField] Transform _root;
+    private float _vSpeed = 0;
+
+    Vector3 _currentMovement;
+    bool _movementPressed;
+    bool _isRunning;
+    private void Reset()
+    {
+        _animator = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
+    }
+    private void Awake()
+    {
+        //Mouvement
+        _movement.action.performed += StartMovement;
+        _movement.action.canceled += StopMovement;
+
+        //Sprint
+        _sprint.action.started += SprintStarted;
+        _sprint.action.performed += SprintUpdate;
+        _sprint.action.canceled += SprintCanceled;
+    }
+
+    private void SprintCanceled(InputAction.CallbackContext obj)
+    {
+        _isRunning = false;
+    }
+
+    private void SprintUpdate(InputAction.CallbackContext obj)
+    {
+        _isRunning = true;
+    }
+
+    private void SprintStarted(InputAction.CallbackContext obj)
+    {
+        _isRunning = true;
+    }
+
+
+    void Start()
+    {
+        _isWalkingAnim = Animator.StringToHash("isWalking");
+    }
+
+
+    void Update()
+    {
+        _animator.SetBool("isRunning", _isRunning);
+        Movement();
+    }
+
+    private void StartMovement(InputAction.CallbackContext obj)
+    {
+        _currentMovement = obj.ReadValue<Vector2>();
+        _movementPressed = true;
+    }
+
+    private void StopMovement(InputAction.CallbackContext obj)
+    {
+        _currentMovement = Vector2.zero;
+        _movementPressed = false;
+    }
+
+    void Movement()
+    {
+        bool _isWalking = _animator.GetBool(_isWalkingAnim);
+        if (_movementPressed)
+        {
+            _animator.SetBool(_isWalkingAnim, true);
+        }
+        else
+        {
+            _animator.SetBool(_isWalkingAnim, false);
+        }
+
+        // Camera
+        var realDirection = new Vector3(_currentMovement.x, 0, _currentMovement.y);
+        realDirection = _camera.transform.TransformDirection(realDirection);
+
+        // Rotation
+        transform.LookAt(transform.position + realDirection);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+
+        // Gravity
+        _vSpeed -= _gravity * Time.deltaTime;
+        realDirection.y = _vSpeed;
+
+        if (_isRunning)
+        {
+            _controller.Move(realDirection * _speed * Time.deltaTime * 2);
+            _isRunning = true;
+        }
+        else
+        {
+            _controller.Move(realDirection * _speed * Time.deltaTime);
+            _isRunning = false;
+        }
+    }
+
+}
