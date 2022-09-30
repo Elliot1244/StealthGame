73938dd6f4518bc9e8fe4f64d3ce79af4621b565
Elliot <elliot.lenoa@laposte.net>
2022-09-30 14:41:48 +0200

    some debug with the AI and refactoring

--- Assets/Scripts/PNJ/Witch/WitchDetection.cs
+++ Assets/Scripts/PNJ/Witch/WitchDetection.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class WitchDetection : MonoBehaviour
 {
+    enum WitchState { IDLE, CHASE, SEARCH }
+
     [SerializeField] Animator _animator;
-    [SerializeField] Transform _player;
     [SerializeField] GameObject _witch;
+    [SerializeField] Rigidbody _witchRb;
     [SerializeField] float  _speed;
     [SerializeField] float _speedChase;
-    bool _seePlayer = false;
-    bool _playerLost = true;
 
+    WitchState _state;
+    PlayerMouvement _playerDetected;
+
+    private void Awake()
+    {
+        _state = WitchState.IDLE;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<PlayerMouvement>())
+        if (other.gameObject.TryGetComponent<PlayerMouvement>(out var m))
         {
+            _state = WitchState.CHASE;
+            _playerDetected = m;
             Debug.Log("joueur entré");
-            _seePlayer = true;
-            _playerLost = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<PlayerMouvement>(out var m))
+        {
+            //Debug.Log("joueur entré");
+            _playerDetected = m;
+            _state = WitchState.CHASE;
         }
     }
 
@@ -27,52 +44,58 @@ public class WitchDetection : MonoBehaviour
     {
         if (other.gameObject.GetComponent<PlayerMouvement>())
         {
+            _state = WitchState.SEARCH;
             Debug.Log("Joueur sorti");
-            _seePlayer = false;
             StartCoroutine("SawPlayer");
         }
     }
 
-    private void Update()
-    {
-        Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
-
-        //Rotation
-        _witch.transform.LookAt(_player);
-        _witch.transform.Rotate(0, 180, 0);
-
-        //If witch sees player
-        if (_playerLost == false)
-        {                                                                                               
-            _animator.SetBool("isRunning", true);
-            Vector3 direction = _witch.transform.position - _player.transform.position;
-            direction = direction.normalized;
-            _witch.transform.position -= direction * Time.deltaTime * _speed;
-
-            if (_seePlayer == true)
-            {
-                //Witch towards player
-                Vector3 directionChase = _witch.transform.position - _player.transform.position;
-                directionChase = directionChase.normalized;
-                _witch.transform.position -= directionChase * Time.deltaTime * _speedChase;
-            }
-            else
-            {
-                _animator.SetBool("isRunning", false);
-            }
-        }
-        else
-        {
-            _animator.SetBool("isRunning", false);
-        }
-    }
-
-
     IEnumerator SawPlayer()
     {
         yield return new WaitForSeconds(5);
-        _playerLost = true;
+        _state = WitchState.IDLE;
+        _playerDetected = null;
         Debug.Log("joueur perdu");
         yield break;
     }
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(_witchRb.position, _witchRb.transform.forward * 5, Color.red);
+
+        //Rotation
+        if(_playerDetected !=null)
+        {
+            _witchRb.transform.LookAt(_playerDetected.transform.position);
+        }
+        
+        Vector3 futurPosition = _witchRb.transform.position;
+        switch (_state)
+        {
+            case WitchState.IDLE:
+                // NO move
+                _animator.SetBool("isRunning", false);
+                break;
+            case WitchState.CHASE:
+                Debug.Log("1");
+                _animator.SetBool("isRunning", true);
+                //Witch towards player
+                Vector3 directionChase = (_playerDetected.transform.position - _witchRb.transform.position).normalized;
+                futurPosition += directionChase * Time.fixedDeltaTime * _speedChase;
+                break;
+            case WitchState.SEARCH:
+                Debug.Log("2");
+                _animator.SetBool("isRunning", false);
+                Vector3 direction = (_playerDetected.transform.position - _witchRb.transform.position).normalized;
+                futurPosition += direction * Time.fixedDeltaTime * _speed;
+                break;
+            default:
+                break;
+        }
+
+        _witchRb.MovePosition(futurPosition);
+    }
+
+
+    
 }
